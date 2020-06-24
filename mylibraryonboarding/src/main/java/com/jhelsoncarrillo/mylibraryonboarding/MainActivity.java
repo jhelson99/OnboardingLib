f51e08bf1ba7290de6dbe9f6cb04859e42a7ac32
Jhelson <jhelsoncarrilloortega@gmail.com>
2020-06-24 00:45:24 -0400

    Initial commit

---
+++ mylibraryonboarding/src/main/java/com/jhelsoncarrillo/mylibraryonboarding/MainActivity.java
@@ -0,0 +1,39 @@
+package com.jhelsoncarrillo.mylibraryonboarding;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.jhelsoncarrillo.mylibraryonboarding.Adapters.OnboardingAdapter;
+
+
+public class MainActivity extends AppCompatActivity
+{
+    private ViewPager viewPagerContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initializeComponents
+        _initializeComponents();
+
+        //initializeAdapter
+        _initializeFragmentAdapter();
+    }
+
+    public void _initializeComponents()
+    {
+        viewPagerContainer = findViewById(R.id.viewPagerContainer);
+    }
+
+    public void _initializeFragmentAdapter()
+    {
+        OnboardingAdapter adapter = new OnboardingAdapter(getSupportFragmentManager());
+        viewPagerContainer.setAdapter(adapter);
+    }
+
+}
